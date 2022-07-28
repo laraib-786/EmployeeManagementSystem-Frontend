4b81a5eac1ec20346fd59a076897efec97de5e0f
laraib-786 <ruk10011997@gmail.com>
2022-07-28 12:15:01 +0530

    Add. header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 import './App.css';
 import ListEmployee from './components/ListEmployee';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
       <Router>
+      <Header></Header>
       <div className='container'>
+       
         <Routes>
           <Route path='/' element={<ListEmployee/>}/>
           <Route path='/employees' element={<ListEmployee/>}/>
---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+export default()=>{
+    return(
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">EMS</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/employees">Employees</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Add Employee</a>
+        </li>
+       
+    
+      </ul>
+    </div>
+  </div>
+</nav>
+    );
+};
