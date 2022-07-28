0c87d1d0c8ac59cd71a9a8e686c9116b83736ddd
laraib-786 <ruk10011997@gmail.com>
2022-07-28 11:22:31 +0530

    Add. Configure routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 
 import './App.css';
 import ListEmployee from './components/ListEmployee';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <div  className='container'>
-     <ListEmployee/>
+    <div>
+      <Router>
+      <div className='container'>
+        <Routes>
+          <Route path='/' element={<ListEmployee/>}/>
+          <Route path='/employees' element={<ListEmployee/>}/>
+         
+        </Routes>
+      </div>
+      </Router>
     </div>
+
   );
 }
 
